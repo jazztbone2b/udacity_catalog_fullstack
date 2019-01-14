a308b1e9f883ac07ec9077f1b768584fd5cfcbcf
jazztbone2b <jazztbone2b@gmail.com>
2019-01-13 19:04:42 -0500

    added functionality to create and delete an item

--- project.py
+++ project.py
@@ -44,12 +44,25 @@ def catalogItems(category_id):
     return render_template('items.html', category=category, items=items)
 
 #Create
-@app.route('/catalog/<int:category_id>/new/')
+@app.route('/catalog/<int:category_id>/new/', methods=['GET', 'POST'])
 def newItem(category_id):
     session = DBSession()
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Items).filter_by(category_id=category.id)
-    return render_template('newItem.html', category=category, items=items)
+    #user_id = session.query(Items).filter_by(user_id=Items.user_id)
+
+    if request.method == 'POST':
+        newItem = Items(
+            item_name=request.form['name'],
+            description=request.form['description'], 
+            category_id=category.id, user_id=1
+            )
+        session.add(newItem)
+        session.commit()
+        flash('New Item Successfully Created')
+        return redirect(url_for('catalogItems', category_id=category_id))
+    else:
+        return render_template('newItem.html', category=category, items=items)
 
 #Edit
 @app.route('/catalog/<int:category_id>/edit/')
@@ -60,14 +73,23 @@ def editCatalogItem(category_id):
     return render_template('editItem.html', category=category, items=items)
 
 #Delete
-@app.route('/catalog/<int:category_id>/delete/')
-def deleteCatalogItem(category_id):
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete/', methods=['GET', 'POST'])
+def deleteCatalogItem(category_id, item_id):
     session = DBSession()
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Items).filter_by(category_id=category.id)
-    return render_template('deleteItem.html', category=category, items=items)
+    itemToDelete = session.query(Items).filter_by(id=item_id).one()
+
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash('Item Deleted Successfully')
+        return redirect(url_for('catalogItems', category_id=category_id))
+    else:
+        return render_template('deleteItem.html', category=category, items=items, item=itemToDelete)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
