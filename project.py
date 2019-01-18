86a1019a9b94aea893b7f52c5b243a4d82c512a0
jazztbone2b <jazztbone2b@gmail.com>
2019-01-18 14:32:44 -0500

    added basic functionality for google log in

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, jsonify, url_for, f
 from flask import session as login_session
 from flask import make_response
 
+from flask_dance.contrib.google import make_google_blueprint, google
+
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 
@@ -16,6 +18,10 @@ import random
 import string
 import requests
 
+#Enable login without HTTPS
+import os 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 
 app = Flask(__name__)
 
@@ -25,10 +31,61 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 
-@app.route('/')
-def Homepage():
-    return "Home Page with default catalog view and login option"
+#Google Login using Flask Dance #####
 
+google_blueprint = make_google_blueprint(
+    client_id = '1059882579334-9pou6o75d96ls5agole7l6apm6vp6p8k.apps.googleusercontent.com',
+    client_secret = 'uZ0J_cLBjEWnX8EXbzn7ZWv9'
+)
+
+app.register_blueprint(google_blueprint, url_prefix='/google_login')
+
+@app.route('/google')
+def googleLogin():
+    if not google.authorized:
+        return redirect(url_for('google.login'))
+    account_info = google.get("/oauth2/v2/userinfo")
+
+    if account_info.ok:
+        account_info_json = account_info.json()
+        return "You are {} on Google".format(account_info_json['name'])
+
+
+
+
+
+#JSON ENDPOINTS Work in Progress ######
+
+@app.route('/catalog/JSON')
+def catalogJSON():
+    session = DBSession()
+    catalog = session.query(Category).all()
+    return jsonify(catalog=[r.serialize for r in catalog])
+
+@app.route('/catalog/users/JSON')
+def usersJSON():
+    session = DBSession()
+    users = session.query(User).all()
+    return jsonify(users=[r.serialize for r in users])
+
+@app.route('/catalog/items/JSON')
+def allItemsJSON():
+    session = DBSession()
+    items = session.query(Items).all()
+    return eval(str(items))
+    #return jsonify(items=[r.serialize for r in items])
+
+@app.route('/catalog/<category_id>/JSON')
+def itemsJSON(category_id):
+    session = DBSession()
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Items).filter_by(category_id=category.id)
+    #return str(items)
+    return jsonify(tems=[r.serialize for r in items])
+
+######################
+
+@app.route('/')
 @app.route('/catalog/')
 def Catalog():
     session = DBSession()
